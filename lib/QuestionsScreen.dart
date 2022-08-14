0d682a8ea370bad3265840a4a9db958c5349e6a2
Giselle <gmol013@hotmail.com>
2022-08-14 00:14:14 -0500

    Added start screen
    Added start screen with language and currency

--- lib/QuestionsScreen.dart
+++ lib/QuestionsScreen.dart
@@ -1,5 +1,6 @@
 import 'package:conditional_questions/conditional_questions.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
@@ -29,6 +30,22 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final _key = GlobalKey<QuestionFormState>();
+
+  @override
+  void initState(){
+    super.initState();
+    print("Hello");
+    getPrefs();
+  }
+
+  void getPrefs() async{
+    final prefs = await SharedPreferences.getInstance();
+// Save the currency
+    await prefs.setString('currency', 'MXN');
+// Get the currency
+    final currency = prefs.getString('currency') ?? '';
+    print(currency);
+  }
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/StartScreen.dart
@@ -0,0 +1,188 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter DropDownButton',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+// Initial Selected Value
+  String languageDropdownValue = "Select Language";
+  String currencyDropdownValue = "Select Currency";
+
+
+// List of items in our dropdown menu
+  var languageDropdownItems = [
+    'Select Language'
+  ];
+
+  var currencyDropdownItems = [
+    'Select Currency'
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    // Do something amazing
+    fetchData();
+  }
+
+  fetchData() async {
+   var currencies = await getCurrencies();
+   var languages = await getLanguages();
+   print(currencies);
+   print(languages);
+   languageDropdownItems = languageDropdownItems + languages;
+   currencyDropdownItems = currencyDropdownItems + currencies;
+  }
+
+  getLanguages() async {
+    var jsonText = await rootBundle.loadString('assets/resources/languages.json');
+    var jsonResponse = json.decode(jsonText);
+    List languages = jsonResponse.toList();
+    List<String> languagesList = [];
+    for (var i = 0; i < languages.length; i++) {
+      languagesList.add(languages[i]["name"] as String);
+    }
+    return languagesList;
+  }
+
+  getCurrencies() async {
+    var jsonText = await rootBundle.loadString('assets/resources/currencies.json');
+    var jsonResponse = json.decode(jsonText);
+    List<String> currencies = jsonResponse.keys.toList();
+    currencies = currencies.map((currency)=>currency.toUpperCase()).toList();
+    return currencies;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Start Page"),
+      ),
+      body: Center(
+        child: FractionallySizedBox(
+          heightFactor: 1,
+          widthFactor: 0.9,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(padding: const EdgeInsets.only(top: 8.0, bottom: 8.0)),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Expanded(child: DropdownButton(
+                    isExpanded:true,
+
+                    // Initial Value
+                    value: languageDropdownValue,
+
+                    // Down Arrow Icon
+                    icon: const Icon(Icons.keyboard_arrow_down),
+
+                    // Array list of items
+                    items: languageDropdownItems.map((String items) {
+                      return DropdownMenuItem(
+                        value: items,
+                        child: Text(items),
+                      );
+                    }).toList(),
+                    // After selecting the desired option,it will
+                    // change button value to selected value
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        languageDropdownValue = newValue!;
+                      });
+                    },
+                  )),
+                ],
+              ),
+              Padding(padding: const EdgeInsets.only(top: 8.0, bottom: 8.0)),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Expanded(child: DropdownButton(
+                    isExpanded:true,
+
+                    // Initial Value
+                    value: currencyDropdownValue,
+
+                    // Down Arrow Icon
+                    icon: const Icon(Icons.keyboard_arrow_down),
+
+                    // Array list of items
+                    items: currencyDropdownItems.map((String items) {
+                      return DropdownMenuItem(
+                        value: items,
+                        child: Text(items),
+                      );
+                    }).toList(),
+                    // After selecting the desired option,it will
+                    // change button value to selected value
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        currencyDropdownValue = newValue!;
+                      });
+                    },
+                  )),
+                  Padding(padding: const EdgeInsets.only(left: 10.0)),
+                  Expanded(child: DropdownButton(
+                    isExpanded:true,
+
+                    // Initial Value
+                    value: currencyDropdownValue,
+
+                    // Down Arrow Icon
+                    icon: const Icon(Icons.keyboard_arrow_down),
+
+                    // Array list of items
+                    items: currencyDropdownItems.map((String items) {
+                      return DropdownMenuItem(
+                        value: items,
+                        child: Text(items),
+                      );
+                    }).toList(),
+                    // After selecting the desired option,it will
+                    // change button value to selected value
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        currencyDropdownValue = newValue!;
+                      });
+                    },
+                  )),
+                ],
+              )
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
+
